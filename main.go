e904f9e4c4ea37487f3095363da4c5d4c496f82b
supratim230184 <67423029+supratim230184@users.noreply.github.com>
2025-12-19 15:07:43 +0000

    Update main.go

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
- /**
+/**
  * Copyright 2023 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -24,11 +24,12 @@ import (
 	"net/http"
 )
 
-echo 'func main() {
-    http.HandleFunc("/blue", blueHandler)
-    http.HandleFunc("/red", redHandler)
-    http.ListenAndServe(":8080", nil)
+func main() {
+	http.HandleFunc("/blue", blueHandler)
+	http.HandleFunc("/red", redHandler)
+	http.ListenAndServe(":8080", nil)
 }
+
 func blueHandler(w http.ResponseWriter, r *http.Request) {
 	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
 	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{0, 0, 255, 255}}, image.ZP, draw.Src)
@@ -41,5 +42,3 @@ func redHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "image/png")
 	png.Encode(w, img)
 }
-
-
